7e80d41013ab25ed28e2b134f1b13fb7d65eea86
Muruganantham Bharathakrishnan <166166883+MurugananthamB@users.noreply.github.com>
2025-02-20 16:42:43 +0530

    Update api.jsx

--- src/components/API/api.jsx
+++ src/components/API/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000"; // Updated to use Render backend
+const baseURL = "https://path-backend.onrender.com"; // Updated to use Render backend
 
 //"http://localhost:5000"
 // https://path-backend.onrender.com
