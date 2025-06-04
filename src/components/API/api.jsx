bd62b9b767c2ab9ea02589933f9928194cb3357c
MurugananthamB <murugananthamanand01212@gmail.com>
2025-06-04 17:02:09 +0530

    final

--- src/components/API/api.jsx
+++ src/components/API/api.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:10000";
+const baseURL = "http://192.168.103.158:5001";
 
 // import.meta.env.VITE_API_URL; // Updated to use Render backend
 
+//192.168.103.158:5001
 
 // https://path-backend.onrender.com
 
-export default axios.create({
+ export default axios.create({
   baseURL,
 });
